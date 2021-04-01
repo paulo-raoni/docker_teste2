de9bcd730af63385cc37e64a6ac5b319e18cc6a8
Paulo Raoni <raoni_jkd@hotmail.com>
2021-03-31 22:07:05 -0300

    modifying files

--- app/controllers/ProjetoDockerController.js
+++ app/controllers/ProjetoDockerController.js
@@ -1,8 +1,37 @@
 var log4js = require('log4js');
 var logger = log4js.getLogger('[ProjetoDockerController]');
 log4js.configure('./config/log4js.json');
+const request = require('request');
 
 exports.retornaMensagemDockerTeste2 = async () => {
+	const dadosObtidos = pegaDados();
 	logger.debug("Iniciou retornaMensagemDockerTeste2");
 	return mensagem = {mensagem: "projeto-docker_teste2"};
 };
+
+
+retornaTweet = async () => {
+	const dadosObtidos = await pegaDados();
+	logger.debug("Iniciou retornaMensagemDockerTeste2");
+	return dadosObtidos;
+};
+
+const pegaDados = () => {
+	return new Promise((resolve, reject) => {
+        const url = "http://localhost:8086/dados/";
+        request({
+            url: url,
+            method: 'GET'
+        }, function (error, response, body) {
+            if (error) {
+                reject(error);
+            } else {
+                if (response.statusCode == 200) {
+                    resolve(JSON.parse(body));
+                } else {
+                    reject(body);
+                }
+            }
+        });
+    })
+}
--- app/routes/ProjetoDockerRoute.js
+++ app/routes/ProjetoDockerRoute.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
 	const controller = require('../controllers/ProjetoDockerController');
 
-	app.get('/mensagem-docker-teste/', controller.getDados);
+	app.get('/mensagem-docker-teste/', controller.retornaMensagemDockerTeste2);
 };
